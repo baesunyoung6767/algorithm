fc907f70e5e9c3f612068b652f89eafcec43250e
baesunyoung6767 <tjsdud6677@gmail.com>
2024-01-24 22:18:30 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	priority_queue<int, vector<int>, greater<int>> q; // 오름차순 
+	int N;
+	cin >> N;
+
+	int input;
+	while (N--) {
+		cin >> input;
+		if (input == 0) {
+			if (!q.empty()) {
+				cout << q.top() << '\n'; // 가장 작은 값 출력
+				q.pop();
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+		else {
+			q.push(input);
+		}
+	}
+}

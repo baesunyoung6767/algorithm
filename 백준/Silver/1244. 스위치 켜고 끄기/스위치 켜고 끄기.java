ff955d73cd7fb3b65a19fef2b21eaaa20d9b8a1f
baesunyoung6767 <tjsdud6677@gmail.com>
2024-01-30 20:31:29 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 136 ms, Memory: 15772 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,52 +1,65 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(in.readLine());
-		int[] arr = new int[N];
+		int[] arr = new int[N+1];
 	
 		String[] readLine = in.readLine().split(" ");
-		for(int t = 0; t<N; t++) {
-			arr[t] = readLine[t].charAt(0) - '0';
+		for(int i = 1; i<=N; i++) {
+			arr[i] = readLine[i-1].charAt(0) - '0';
 		}
 		
 		int personNum = Integer.parseInt(in.readLine());
-		int gender;
-		int number;
-		for(int t = 0; t<personNum; t++) {
+		int[][] person = new int[personNum][personNum];
+		
+		for(int i = 0; i<personNum; i++) {
 			String[] personRead = in.readLine().split(" ");
-			gender = personRead[0].charAt(0) - '0';
-			number = Integer.parseInt((personRead[1]));
-			
-			if(gender == 1) { // 남학생인 경우
-				for(int i = 0; i<N; i++) {
-					if((i+1) % number == 0) arr[i] = arr[i] == 0 ? 1 : 0;
-				}
-			}
-			
-			else if(gender == 2) { // 여학생인 경우
-				arr[number - 1] = arr[number - 1] == 0 ? 1 : 0;
-				
-				for(int i = 1; i<=N/2; i++) {
-					if(number - i - 1 < 0 || number + i - 1 >= N) break; // 범위 벗어나면 종료
-					if(arr[number - i - 1] == arr[number + i - 1]) {
-						arr[number - i - 1] = arr[number - i - 1] == 0 ? 1 : 0;
-						arr[number + i - 1] = arr[number + i - 1] == 0 ? 1 : 0;
+			int gender = personRead[0].charAt(0) - '0';
+            int num = Integer.parseInt(personRead[1]);
+            
+            if(gender == 1) { // 남학생인 경우
+				int cnt = 1;
+				for(int j = 1; j<=N; j++) {
+					if(j % num == 0) {
+						if(arr[j] == 0) arr[j] = 1;
+						else arr[j] = 0;
 					}
-					else break;
+				}
+				
+			} else { // 여학생인 경우
+				int left = num - 1;
+				int right = num + 1;
+				
+				while(left > 0 && right <= N) {
+					if(arr[left] != arr[right]) {
+						break;
+					}
+					else {
+						left--;
+						right++;
+					}
+				}
+				for(int j = left+1; j<=right-1; j++) {
+					if(arr[j] == 0) arr[j] = 1;
+					else arr[j] = 0;
 				}
 			}
 		}
 		
 		
-		for(int t = 0; t<N; t++) {
-			sb.append(arr[t] + " ");
-			if((t+1) % 20 == 0) sb.append("\n");
+		
+		for(int i = 1; i<=N; i++) {
+			sb.append(arr[i] + " ");
+			if(i % 20 == 0) sb.append("\n");
 		}
 		sb.append("\n");
 		

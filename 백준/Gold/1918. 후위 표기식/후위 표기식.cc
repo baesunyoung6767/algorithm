b94503a970294fe36a20cee7db271578251ef18f
baesunyoung6767 <tjsdud6677@gmail.com>
2024-02-06 22:14:02 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	string input;
+	cin >> input;
+
+	stack<char> s;
+
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] >= 'A' && input[i] <= 'Z') cout << input[i];
+		else if (input[i] == '(') s.push(input[i]);
+		else if (input[i] == '*' || input[i] == '/') {
+			if (!s.empty() && (s.top() == '*' || s.top() == '/')) {
+				cout << s.top();
+				s.pop();
+			}
+			s.push(input[i]);
+		}
+		else if (input[i] == '+' || input[i] == '-') {
+			while (!s.empty() && s.top() != '(') {
+				cout << s.top();
+				s.pop();
+			}
+			s.push(input[i]);
+		}
+		else if (input[i] == ')') {
+			while (s.top() != '(') {
+				cout << s.top();
+				s.pop();
+			}
+			s.pop();
+		}
+
+	}
+
+	while (!s.empty()) {
+		cout << s.top();
+		s.pop();
+	}
+
+}

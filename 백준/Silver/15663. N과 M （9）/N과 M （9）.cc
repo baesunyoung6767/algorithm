89cdc29e043ac34c00b575eb8c0928ba48f164b8
baesunyoung6767 <tjsdud6677@gmail.com>
2024-02-25 02:15:39 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,44 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int N, M;
-vector<int> v;
-int result[101] = { 0 };
-int visited[101] = { 0 };
-// 1 7 7 9
+int n, m;
+int arr[10];
+int num[10];
+int check[10];
 
-
-// 중복 제거
-void output(int cnt) {
-	if (cnt == M) {
-		for (int i = 0; i < M; i++) {
-			cout << result[i] << ' ';
-		}
+void seq(int len) {
+	if (len == m) {
+		for (int i = 0; i < m; i++) cout << arr[i] << ' ';
 		cout << '\n';
+		return;
 	}
 
 	int last = 0;
-	for (int i = 0; i < v.size(); i++) {
-		if (visited[i] == 1 || last == v[i]) continue;
-		result[cnt] = v[i];
-		last = v[i];
-		visited[i] = 1;
-		output(cnt + 1);
-		visited[i] = 0;
+	for (int i = 0; i < n; i++) {
+		if (check[i] == 0 && num[i] != last) {
+			arr[len] = num[i];
+			last = arr[len];
+			check[i] = 1;
+			seq(len + 1);
+			check[i] = 0;
+		}
 	}
+	return;
 }
 
 int main() {
-	cin >> N >> M;
-
-	int tmp;
-	for (int i = 0; i < N; i++) {
-		cin >> tmp;
-		v.push_back(tmp);
-	}
-	sort(v.begin(), v.end());
-	output(0);
-    return 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> num[i];
+	sort(num, num + n);
+	seq(0);
+	return 0;
 }

c469eda37182661d68be38d67f653e10fe71698a
baesunyoung6767 <tjsdud6677@gmail.com>
2024-02-25 15:27:15 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 52 ms, Memory: 12584 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,35 +1,33 @@
-#include <iostream>
-#include <vector>
-#define MAX 100001
-using namespace std;
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+#define MAX 100001 
 
-int N;
-vector<int> v[MAX];
-int visited[MAX] = { 0 };
-int result[MAX] = { 0 };
+int N; 
+int arr[MAX]; 
+bool visited[MAX]; 
+vector<int> v[MAX]; 
 
-void dfs(int cur) {
-	visited[cur] = 1;
-	for (int i = 0; i < v[cur].size(); i++) {
-		if (visited[v[cur][i]] == 1) continue;
-		visited[v[cur][i]] = 1;
-		result[v[cur][i]] = cur;
-		dfs(v[cur][i]);
-	}
-}
+void dfs(int k) { 
+	visited[k]=true; 
+	for(int i=0;i<v[k].size();i++) { 
+		int next = v[k][i]; 
+		if(!visited[next]) { 
+			arr[next]=k; dfs(next); 
+		} 
+	} 
+} 
 
-int main() {
-	cin >> N;
-
-	int a, b;
-	for (int i = 1; i < N; i++) {
-		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}
-
-	dfs(1);
-	for (int i = 2; i <= N; i++) {
-		cout << result[i] << '\n';
-	}
+int main() { 
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N; 
+	for(int i=0;i<N;i++) { 
+		int x,y; cin >> x >> y; 
+		v[x].push_back(y); 
+		v[y].push_back(x); 
+	} 
+	
+	dfs(1); 
+	
+	for(int i=2;i<=N;i++) cout << arr[i] << "\n"; 
 }

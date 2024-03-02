8d04e134d99af4dbc697565bfe8c5eb1d5342cf6
baesunyoung6767 <tjsdud6677@gmail.com>
2024-03-03 01:05:27 +0900

    [Gold IV] Title: 감시, Time: 40 ms, Memory: 2260 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,105 +1,105 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 #define MAX 101
 using namespace std;
 
-vector<pair<int, int>> v;
-int x[] = { 1, 0, -1, 0 };
-int y[] = { 0, -1, 0, 1 };
-int map[MAX][MAX] = { 0 };
-int output = 987654321;
 int N, M;
+int arr[MAX][MAX] = { 0 };
+vector<pair<int, int>> v;
 
-void check(int cy, int cx, int d) {
-    d %= 4;
-    while (1) {
-        int ny = cy + y[d];
-        int nx = cx + x[d];
-        cy = ny; cx = nx;
-        if (ny < 0 || nx < 0 || ny >= N || nx >= M || map[ny][nx] == 6) break;
-        if (map[ny][nx] != 0) continue;
-        map[ny][nx] = -1;
-    }
+int x[] = { 1, 0, -1, 0 };
+int y[] = { 0, 1, 0, -1 };
+
+int result = 987654321;
+
+void dfs(int dir, int startY, int startX) {
+	int cy = startY;
+	int cx = startX;
+
+	while (1) {
+		int ny = cy + y[dir];
+		int nx = cx + x[dir];
+		if (ny < 0 || nx < 0 || ny >= N || nx >= M || arr[ny][nx] == 6) break;
+		if (arr[ny][nx] == 0) arr[ny][nx] = 7;
+		cy = ny;
+		cx = nx;
+	}
 }
 
-void dfs(int cnt) {
-    if (cnt == v.size()) { // 모든 cctv에 대한 탐색 끝
-        int count = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) count++;
-            }
-        }
+void check(int cnt) {
+	if (cnt == v.size()) {
+		int cnt = 0;
 
-        output = min(output, count);
-        return;
-    }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] == 0) cnt++;
+			}
+		}
 
-    int cy = v[cnt].first;
-    int cx = v[cnt].second;
-    int dir = map[cy][cx];
-    int temp[MAX][MAX] = { 0 };
-    for (int d = 0; d < 4; d++) { // 우 상 좌 하
-        // 배열 임의 저장
+		result = min(result, cnt);
+		return;
+	}
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                temp[i][j] = map[i][j];
-            }
-        }
+	
+		int tempY = v[cnt].first;
+		int tempX = v[cnt].second;
+		int kind = arr[tempY][tempX];
+		int copyArr[MAX][MAX] = { 0 };
 
-        switch (dir) {
-        case 1:
-            check(cy, cx, d);
-            break;
-        case 2:
-            check(cy, cx, d);
-            check(cy, cx, d + 2);
-            break;
-        case 3:
-            check(cy, cx, d);
-            check(cy, cx, d + 1);
-            break;
-        case 4:
-            check(cy, cx, d);
-            check(cy, cx, d + 1);
-            check(cy, cx, d + 2);
-            break;
-        case 5:
-            check(cy, cx, d);
-            check(cy, cx, d + 1);
-            check(cy, cx, d + 2);
-            check(cy, cx, d + 3);
-            break;
-        default:
-            break;
-        }
+		// 맵 복사
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				copyArr[i][j] = arr[i][j];
+			}
+		}
 
-        dfs(cnt + 1);
+		for (int i = 0; i < 4; i++) {
+			switch (kind) {
+			case 1:
+				dfs((0 + i) % 4, tempY, tempX);
+				break;
+			case 2:
+				dfs((0 + i) % 4, tempY, tempX);
+				dfs((2 + i) % 4, tempY, tempX);
+				break;
+			case 3:
+				dfs((0 + i) % 4, tempY, tempX);
+				dfs((3 + i) % 4, tempY, tempX);
+				break;
+			case 4:
+				dfs((0 + i) % 4, tempY, tempX);
+				dfs((2 + i) % 4, tempY, tempX);
+				dfs((3 + i) % 4, tempY, tempX);
+				break;
+			case 5:
+				dfs((0 + i) % 4, tempY, tempX);
+				dfs((1 + i) % 4, tempY, tempX);
+				dfs((2 + i) % 4, tempY, tempX);
+				dfs((3 + i) % 4, tempY, tempX);
+				break;
+			}
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                map[i][j] = temp[i][j];
-            }
-        }
-    }
+			check(cnt + 1);
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					arr[i][j] = copyArr[i][j];
+				}
+			}
+		}
 }
 
 int main() {
-    cin >> N >> M;
+	cin >> N >> M;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == 1 || map[i][j] == 2 || map[i][j] == 3 || map[i][j] == 4 || map[i][j] == 5) {
-                v.push_back({ i, j });
-            }
-        }
-    }
-
-    dfs(0);
-    cout << output << '\n';
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> arr[i][j];
+			if (arr[i][j] != 0) 
+				if(arr[i][j] != 6) v.push_back({ i,j }); // 카메라 위치 저장
+			}
+	}
+	check(0);
+	cout << result << '\n';
 }
-

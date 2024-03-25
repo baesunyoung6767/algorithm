98b11d1cb20fd8bc49520c7c278e5dc745844e7e
baesunyoung6767 <tjsdud6677@gmail.com>
2024-03-25 23:40:57 +0900

    Update 여행 가자.cc

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -1,52 +1,41 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<cstring>
-#include<stack>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int n,m,cnt;
-vector<int> v[204];
-bool vst[204];
+int N, M;
+vector<int> v[201];
+int visited[201] = { 0 };
 
-void dfs(int k){
-	vst[k] = 1;
-	for(int e: v[k]){
-		if(!vst[e]){
-			dfs(e);
-		}
+void dfs(int node) {
+	for (int i = 0; i < v[node].size(); i++) {
+		if (visited[v[node][i]] == 1) continue;
+		visited[v[node][i]] = 1;
+		dfs(v[node][i]);
 	}
-	return;
 }
 
-int main(void) {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	
-	cin >> n >> m;
-	for(int i=1; i<=n; i++){
-		for(int j=1; j<=n; j++){
-			int x;
-			cin >> x;
-			if(x == 1) {
-				v[i].push_back(j);
-			}
+int main() {
+	cin >> N >> M;
+	int temp;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> temp;
+			if (temp == 1) v[i].push_back(j);
 		}
 	}
-	
-	
-	for(int i=0; i < m; i++){
-		int x;
-		cin >> x;
-		if(!vst[x]){
+    
+    int cnt = 0;
+	for (int i = 0; i < M; i++) {
+		cin >> temp;
+		if (visited[temp] == 0) {
+            visited[temp] = 1;
 			cnt++;
-			dfs(x);
+            dfs(temp);
 		}
 	}
-	
-	if(cnt == 1) cout << "YES";
-	else cout << "NO";
-	return 0;
+
+	if(cnt == 1) cout <<"YES\n";
+    else cout <<"NO\n";
+
 }

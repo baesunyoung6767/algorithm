f434cf3d31059ead5f6ad2e075595f1a8e3cfd92
baesunyoung6767 <tjsdud6677@gmail.com>
2024-03-27 21:18:52 +0900

    [Silver II] Title: 슬라임 잡고 레벨 업!, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/29717. 슬라임 잡고 레벨 업！/슬라임 잡고 레벨 업！.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+long long T, N;
+int main() {
+
+    cin >> T;
+
+    while (T--) {
+        cin >> N;
+        long long exp = N * (N + 1) / 2; // n마리 처치 후 얻을 수 있는 경험치
+
+        long long start = 1;
+        long long end = 1500000000;
+        long long mid = 0;
+        long long result = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+
+            // mid 레벨까지 오는데 필요한 경험치
+            long long need = mid * mid - mid;
+
+            if (exp < need) end = mid - 1;
+            else {
+                start = mid + 1;
+                result = mid;
+            }
+        }
+        cout << result << '\n';
+
+    }
+}

24a49db04544d4a0ea72895de597b375b990219f
baesunyoung6767 <tjsdud6677@gmail.com>
2024-04-02 21:03:15 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 1216 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 int N, M;
 char map[MAX][MAX] = { 0 };
-// 0 ~ 5 : a ~ f 열쇠
+// 1 ~ 6 : a ~ f 열쇠
 int x[] = { 1, 0, -1, 0 };
 int y[] = { 0, 1, 0, -1 };
 int result = 987654321;
@@ -30,26 +30,26 @@ void bfs(int startY, int startX, int time) {
             int ny = cy + y[i];
             int nx = cx + x[i];
 
-            if (ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx] == 1) continue;
+            if (ny < 0 || nx < 0 || ny >= N || nx >= M || map[ny][nx] == '#' || visited[ny][nx] == 1) continue;
 
             if (map[ny][nx] == '1' && ctime + 1 < result) {
                 result = ctime + 1;
                 return;
             }
 
-            if(map[ny][nx] == '.' || map[ny][nx] == '0')  q.push({ {ny, nx}, ctime + 1 });
             if (map[ny][nx] == 'a' || map[ny][nx] == 'b' || map[ny][nx] == 'c' || map[ny][nx] == 'd' || map[ny][nx] == 'e' || map[ny][nx] == 'f') {
                 if (checkKey[map[ny][nx] - 'a'] == 0) {
                     checkKey[map[ny][nx] - 'a'] = 1;
                     bfs(ny, nx, ctime + 1);
                     checkKey[map[ny][nx] - 'a'] = 0;
                 }
-                else  q.push({ {ny, nx}, ctime + 1 });
+                else q.push({ {ny, nx}, ctime + 1 });
             }
             if (map[ny][nx] == 'A' || map[ny][nx] == 'B' || map[ny][nx] == 'C' || map[ny][nx] == 'D' || map[ny][nx] == 'E' || map[ny][nx] == 'F') {
                 if (checkKey[map[ny][nx] - 'A'] == 0) continue;
-                else  q.push({ {ny, nx}, ctime + 1 });
+                else q.push({ {ny, nx}, ctime + 1 });
             }
+            if(map[ny][nx] == '.')  q.push({ {ny, nx}, ctime + 1 });
             visited[ny][nx] = 1;
         }
 
@@ -65,6 +65,7 @@ int main() {
             cin >> map[i][j];
             if (map[i][j] == '0') {
                 startY = i; startX = j;
+                map[i][j] = '.';
             }
         }
     }

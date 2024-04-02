07df3bdd8419eb6df143f1d5b93780eb4ee718d3
baesunyoung6767 <tjsdud6677@gmail.com>
2024-04-02 20:58:58 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 1240 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
@@ -1,100 +1,75 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <queue>
+
+#define MAX 51
 using namespace std;
-#define MAXN 51
-#define t tuple<int, int, int>
 
-int n, m, result = INT_MAX, dir[2][4] = { {-1, 1, 0, 0}, {0, 0, -1, 1}};
-char maze[MAXN][MAXN];
-bool keys[26];
+int N, M;
+char map[MAX][MAX] = { 0 };
+// 0 ~ 5 : a ~ f 열쇠
+int x[] = { 1, 0, -1, 0 };
+int y[] = { 0, 1, 0, -1 };
+int result = 987654321;
+int checkKey[6] = { 0 };
 
-bool check_range(int y, int x) {
-	if (y < n && x < m && y >= 0 && x >= 0) return true;
-	return false;
-}
+void bfs(int startY, int startX, int time) {
+    int visited[MAX][MAX] = { 0 };
+    queue<pair<pair<int, int>, int>> q;
+    q.push({ {startY, startX}, time });
+    visited[startY][startX] = 1;
 
-void bfs(t start) {
-	int y, x, time;
-	tie(y, x, time) = start;
-	bool visited[MAXN][MAXN] = {false, };
-	queue<t> q;
-	q.push(start);
-	visited[y][x] = true;
+    while (!q.empty()) {
+        int cy = q.front().first.first;
+        int cx = q.front().first.second;
+        int ctime = q.front().second;
+        q.pop();
 
-	while (q.size()) {
-		int cy, cx, ctime;
-		tie(cy, cx, ctime) = q.front(); q.pop();
+        if (result != 987654321 && ctime > result) break;
 
-		if (result != INT_MAX && ctime > result) break;
+        for (int i = 0; i < 4; i++) {
+            int ny = cy + y[i];
+            int nx = cx + x[i];
 
-		for (int i = 0; i < 4; i++) {
-			int ny = cy + dir[0][i], nx = cx + dir[1][i];
+            if (ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx] == 1) continue;
 
-			if (check_range(ny, nx) && !visited[ny][nx]) {
+            if (map[ny][nx] == '1' && ctime + 1 < result) {
+                result = ctime + 1;
+                return;
+            }
 
-				// 다음 위치가 목적지라면
-				if (maze[ny][nx] == '1' && ctime + 1 < result) {
-					result = ctime + 1;
-					return;
-				}
+            if(map[ny][nx] == '.' || map[ny][nx] == '0')  q.push({ {ny, nx}, ctime + 1 });
+            if (map[ny][nx] == 'a' || map[ny][nx] == 'b' || map[ny][nx] == 'c' || map[ny][nx] == 'd' || map[ny][nx] == 'e' || map[ny][nx] == 'f') {
+                if (checkKey[map[ny][nx] - 'a'] == 0) {
+                    checkKey[map[ny][nx] - 'a'] = 1;
+                    bfs(ny, nx, ctime + 1);
+                    checkKey[map[ny][nx] - 'a'] = 0;
+                }
+                else  q.push({ {ny, nx}, ctime + 1 });
+            }
+            if (map[ny][nx] == 'A' || map[ny][nx] == 'B' || map[ny][nx] == 'C' || map[ny][nx] == 'D' || map[ny][nx] == 'E' || map[ny][nx] == 'F') {
+                if (checkKey[map[ny][nx] - 'A'] == 0) continue;
+                else  q.push({ {ny, nx}, ctime + 1 });
+            }
+            visited[ny][nx] = 1;
+        }
 
-				// 다음 위치가 빈 공간이라면
-				else if (maze[ny][nx] == '.' || maze[ny][nx] == '0') {
-					q.push({ ny, nx, ctime + 1 });
-				}
-
-				// 다음 위치가 키라면
-				else if (maze[ny][nx] >= 'a' && maze[ny][nx] <= 'z') {
-
-					// 키를 이미 가지고 있다면
-					if (keys[maze[ny][nx] - 'a']) {
-						q.push({ ny, nx, ctime + 1 });
-					}
-					// 키가 없다면
-					else {
-						keys[maze[ny][nx] - 'a'] = true;
-						bfs({ ny, nx, ctime + 1 });
-						keys[maze[ny][nx] - 'a'] = false;
-					}
-				}
-
-				// 다음 위치가 문이라면
-				else if (maze[ny][nx] >= 'A' && maze[ny][nx] <= 'Z') {
-					// 키를 가지고 있다면
-					if (keys[maze[ny][nx] - 'A']) {
-						q.push({ ny, nx, ctime + 1 });
-					}
-					// 키가 없다면
-					else {
-						continue;
-					}
-				}
-
-				visited[ny][nx] = true;
-			}
-		}
-	}
+    }
 }
 
 int main() {
-	cin >> n >> m;
+    cin >> N >> M;
 
-	t start;
+    int startY = 0, startX = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> map[i][j];
+            if (map[i][j] == '0') {
+                startY = i; startX = j;
+            }
+        }
+    }
 
-	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < m; k++) {
-			cin >> maze[i][k];
-			if (maze[i][k] == '0') {
-				start = make_tuple(i, k, 0);
-			}
-		}
-	}
-
-	bfs(start);
-
-	if (result != INT_MAX) {
-		cout << result;
-	}
-	else {
-		cout << "-1";
-	}
+    bfs(startY, startX, 0);
+    if (result == 987654321) cout << -1 << '\n';
+    else cout << result << '\n';
 }

79d2b20c778bee09852ade76f516306f108a3574
baesunyoung6767 <tjsdud6677@gmail.com>
2024-05-27 21:24:37 +0900

    [level 3] Title: 불량 사용자, Time: 0.04 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -2,48 +2,49 @@
 #include <vector>
 #include <set>
 #include <iostream>
+#define MAX 9
 using namespace std;
 
-vector<vector<int>> userIdList;
-set<set<int>> result;
+vector<string>user_list[MAX];
+set<set<string>> result;
 
-bool comIsPos(string a, string b) { // 아이디, 불량 사용자
-    for(int i = 0; i<a.size(); i++) {
-        if(a[i] != b[i] && b[i] != '*') return false;
-    }    
-    return true;
-} 
-
-void calcResult(int cnt, set<int> data) {
-    if(cnt >=0) {
-        for(int i = 0; i < userIdList[cnt].size(); i++) {
-            if(data.find(userIdList[cnt][i]) == data.end()) {
-                data.insert(userIdList[cnt][i]);
-                calcResult(cnt-1, data);
-                data.erase(userIdList[cnt][i]);
+void checkResult(int cnt, set<string> temp) {
+    if(cnt >= 0) {
+        for(int i = 0; i<user_list[cnt].size(); i++) {
+            if(temp.find(user_list[cnt][i]) == temp.end()) {
+                temp.insert(user_list[cnt][i]);
+                checkResult(cnt-1, temp);
+                temp.erase(user_list[cnt][i]);
             }
         }
     } else {
-        result.insert(data);
+        // 하나의 경우의 수 완성
+        result.insert(temp);
     }
+}
+
+bool isPos(string user, string banned) {
+    for(int i = 0; i<user.size(); i++) {
+        if(user[i] != banned[i] && banned[i] != '*') return false;
+    }    
+    return true;
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
     
-    userIdList = vector<vector<int>> (banned_id.size());
+    // banned_id에 가능한 user_id 찾기
     for(int i = 0; i<banned_id.size(); i++) {
         for(int j = 0; j<user_id.size(); j++) {
-            if(user_id[j].size() == banned_id[i].size()) {
-                if(comIsPos(user_id[j], banned_id[i])) {
-                    userIdList[i].push_back(j);
-                }
+            if(banned_id[i].size() != user_id[j].size()) continue;
+            if(isPos(user_id[j], banned_id[i])) {
+                user_list[i].push_back(user_id[j]);
             }
         }
     }
     
-    set<int> temp;
-    calcResult(banned_id.size() - 1, temp);
+    set<string> s;
+    checkResult(banned_id.size()-1, s);
     answer = result.size();
     return answer;
 }

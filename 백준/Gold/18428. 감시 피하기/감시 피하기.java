596375dad8b946497fdb5ab90557ef0e2d95f634
SunYoung <tjsdud6677@gmail.com>
2025-07-25 23:59:16 +0900

    [Gold V] Title: 감시 피하기, Time: 160 ms, Memory: 18360 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static class pos {
+        int x;
+        int y;
+
+        public pos(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    public static BufferedReader bf = new BufferedReader(new InputStreamReader((System.in)));
+    public static int N;
+    public static char[][] map;
+    public static ArrayList<pos> teacherPos;
+    public static int[] x = {0, 1, 0, -1};
+    public static int[] y = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws Exception{
+        N = Integer.parseInt(bf.readLine());
+        map = new char[N + 1][N + 1];
+        teacherPos = new ArrayList<>();
+
+        for(int i = 0; i<N; i++) {
+            String[] input = bf.readLine().split(" ");
+            for(int j = 0; j<N; j++) {
+                map[i][j] = input[j].charAt(0);
+                if(map[i][j] == 'T') {
+                    teacherPos.add(new pos(i, j));
+                }
+            }
+        }
+
+        putObj(0);
+
+        System.out.println("NO");
+    }
+
+    public static void putObj(int cnt) {
+        if(cnt == 3) {
+            boolean isPos = checkStu();
+            if(isPos) {
+                System.out.println("YES");
+                System.exit(0);
+            }
+            return;
+        }
+
+        for(int i = 0; i<N; i++) {
+            for(int j = 0; j<N; j++) {
+                if(map[i][j] == 'X') {
+                    map[i][j] = 'O';
+                    putObj(cnt + 1);
+                    map[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public static boolean checkStu() {
+        boolean isPos = true;
+
+        for(int i = 0; i<teacherPos.size(); i++) {
+            int cy = teacherPos.get(i).y;
+            int cx = teacherPos.get(i).x;
+
+            for(int j = 0; j<4; j++) {
+                int ny = cy + y[j];
+                int nx = cx + x[j];
+                while(true) {
+                    if(ny < 0 || nx < 0 || ny >= N || nx >= N) break;
+                    if(map[ny][nx] == 'O') break;
+                    if(map[ny][nx] == 'S') {
+                        isPos = false;
+                        break;
+                    }
+                    ny += y[j];
+                    nx += x[j];
+                }
+                if(!isPos) break;
+            }
+        }
+        return isPos;
+     }
+}
